9909c69af725a911e4ac15758939b7502da982e1
gozillatiamo <gozilla.tiamo@gmail.com>
2024-12-14 20:52:59 +0700

    add branches project

---
+++ functions/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    print_labeled_measurement(5, 'h');
+
+    let x = five();
+
+    println!("The value of x is: {x}");
+
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+// Function Expression with return value
+fn five() -> i32 {
+    5
+}
+
+// Function Expression with parameter
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
